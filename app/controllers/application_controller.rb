9ae1be40df7802a766f4fc1e43658917bbe1cf88
kusaba akira <shimau01@gmail.com>
2015-11-13 05:38:00 +0000

    add cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :now
+
+  def now
+    @time = Time.now
+  end
+
+  def whitelist
+    %w{store product}.include?(controller_name)
+  end
+
+  private
+
+  def current_cart
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,27 @@
 class Product < ActiveRecord::Base
-validates :title, :description, :image_url, presence: true
-validates :price, numericality: {greater_than_or_equal_to: 0.01}
-validates :title, uniqueness: true
-validates :image_url, allow_blank: true, format:{
-  with: %r{\.(gif|jpg|png)\z}i,
-  message: 'はGIF、JPG、PNG、画像のURLでなければなりません。'
-}
-end
+
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format:{
+    with: %r{\.(gif|jpg|png)\z}i,
+    message: 'はGIF、JPG、PNG、画像のURLでなければなりません。'
+  }
+  validates :title, length: { minimum: 10 ,
+    too_short:"%{count}文字以上で入力してください。"}
+
+    private
+
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        error.add(:base, '品目が存在します')
+        return false
+      end
+    end
+  end
