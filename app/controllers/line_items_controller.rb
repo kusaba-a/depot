b01a7ec7d5cc8923fad20e0b531f015230ff606e
kusaba akira <shimau01@gmail.com>
2015-11-16 04:59:53 +0000

    add quantity to line_items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,7 +27,7 @@ class LineItemsController < ApplicationController
     @cart = current_cart
     product = Product.find(params[:product_id])
 
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product.id)
 
     # @line_item = LineItem.new
     # @line_item.product_id = product.id
@@ -40,7 +40,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice:  'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart}
         format.json { render :show, status: :created, location: @line_item }
         # format.json { render json :@line_item, status: :created, location: @line_item }
       else
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,5 +3,22 @@ class StoreController < ApplicationController
 
   def index
     @products = Product.order(:title)
+
+    #デバッグ用
+    puts"----------------"
+    puts session[:counter]
+    puts"----------------"
+
+    #1からカウント
+
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
+    @counter = session[:counter]
+
+    #5からカウント
+
   end
 end
